44b783251b8c23a4d827504c284ec44c19f6696b
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-22 22:46:29 -0300

    feat: building the Paciente table

--- src/main/java/org/ifpb/model/Paciente.java
+++ src/main/java/org/ifpb/model/Paciente.java
@@ -1,17 +1,56 @@
 package org.ifpb.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Objects;
 
+@NoArgsConstructor
+@Getter @Setter
+@Entity
 public class Paciente {
-    private String nome;
-    private String cpf;
-    private String rg;
-    private String telefone;
-    private String email;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+
+    @Column(length=11, nullable=false, unique = true)
+    private String cpf;
+
+    private String telefone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
     private int datanascimento;
 
-    public Paciente(){}
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Paciente paciente = (Paciente) o;
+        return Objects.equals(cpf, paciente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cpf);
+    }
 
 
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", datanascimento=" + datanascimento +
+                '}';
+    }
 }
