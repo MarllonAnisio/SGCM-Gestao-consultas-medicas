7bbc99c403effb31ac2dd56c91bf0d840615dc91
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-24 22:35:47 -0300

    feat: created Consulta.class

---
+++ src/main/java/org/ifpb/model/Consulta.java
@@ -0,0 +1,32 @@
+package org.ifpb.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.ifpb.model.enums.StatusConsulta;
+
+import java.time.LocalDateTime;
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "consultas")
+@Entity
+public class Consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private LocalDateTime data;
+
+    @Column(name = "observacoes")
+    private String observacao;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusConsulta status = StatusConsulta.AGENDADA;
+
+
+}
