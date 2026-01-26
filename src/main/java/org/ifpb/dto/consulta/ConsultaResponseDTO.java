7bb9260ab02cabce7f2f7b79f80c29617b92d644
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-25 22:56:32 -0300

    docs: ConsultaResponseDTO

--- src/main/java/org/ifpb/dto/consulta/ConsultaResponseDTO.java
+++ src/main/java/org/ifpb/dto/consulta/ConsultaResponseDTO.java
@@ -9,6 +9,19 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+/**
+ * @class ConsultaResponseDTO
+ * utilizing to transfer data from model to controller
+ * @var id is the id of the consultation
+ * @var data is the date of the consultation
+ * @var status is the status of the consultation
+ * @var observacao is the observation of the consultation
+ * @var idPaciente is the id of the patient
+ * @var nomePaciente is the name of the patient
+ * @var cpfPaciente is the CPF(Brazilian Tax ID) of the patient
+ * @var idMedico is the id of the doctor
+ * @var NomeMedico is the name of the doctor
+ * */
 public class ConsultaResponseDTO {
 
     private int id;
