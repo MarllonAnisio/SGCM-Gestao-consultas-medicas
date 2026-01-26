5f685aed0c11f528976b0b7e919e9f07a83f0c9f
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-25 22:57:27 -0300

    docs: PacienteRequestDTO

--- src/main/java/org/ifpb/dto/paciente/PacienteRequestDTO.java
+++ src/main/java/org/ifpb/dto/paciente/PacienteRequestDTO.java
@@ -12,6 +12,11 @@ import lombok.Setter;
 /**
  * @Class PacienteRequestDTO
  * utilizing to transfer data from controller to model
+ * @var nome is the name of the patient
+ * @var cpf is the CPF(Brazilian Tax ID) of the patient
+ * @var telefone is the phone number of the patient
+ * @var email is the email of the patient
+ * @var datanascimento is the birth date of the patient
  * */
 public class PacienteRequestDTO {
 
