75ac1f6e97efe358e60cc1de37b8e04b3de77cf7
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-25 22:04:52 -0300

    refactor: Especialidade  to make the class smarter

--- src/main/java/org/ifpb/model/enums/Especialidades.java
+++ src/main/java/org/ifpb/model/enums/Especialidades.java
@@ -1,15 +1,23 @@
 package org.ifpb.model.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Especialidades {
-    CARDIOLOGIA,
-    DERMATOLOGIA,
-    GINECOLOGIA,
-    PEDIATRIA,
-    ORTOPEDIA,
-    UROLOGIA,
-    NEUROLOGIA,
-    GERIATRIA,
-    FISIOTERAPIA,
-    OBSTETRIA,
-    CLINICA_GERAL;
+    CARDIOLOGIA("Cardiologia"),
+    DERMATOLOGIA("Dermatologia"),
+    GINECOLOGIA("Ginecologia"),
+    PEDIATRIA("Pediatria"),
+    ORTOPEDIA("Ortopedia"),
+    UROLOGIA("Urologia"),
+    NEUROLOGIA("Neurologia"),
+    GERIATRIA("Geriatria"),
+    PSIQUIATRIA("Psiquiatria"),
+    FISIOTERAPIA("Fisioterapia"),
+    OBSTETRIA("Obstetricia"),
+    CLINICA_GERAL("Clinica Geral");
+
+    private final String descricao;
 }
