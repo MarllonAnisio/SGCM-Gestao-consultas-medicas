e95f65156334202b23e0779184cc0bba27b2cfd0
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-10 17:57:10 -0300

    feat: add GenericDAOImpl class for generic CRUD operations

---
+++ src/main/java/org/ifpb/dao/GenericDAOImpl.java
@@ -0,0 +1,54 @@
+package org.ifpb.dao;
+
+import jakarta.persistence.EntityManager;
+import org.ifpb.config.HibernateUtil;
+
+import java.util.List;
+
+public abstract class GenericDAOImpl<T, ID> implements GerericDAO<T, ID> {
+
+    private final Class<T> classe;
+
+    public GenericDAOImpl(Class<T> classe) {
+        this.classe = classe;
+    }
+    protected EntityManager getEntityManager(){
+        return HibernateUtil.getEntityManager();
+    }
+
+
+    @Override
+    public T save(T entity) {
+        return null;
+    }
+
+    @Override
+    public T findById(ID id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(ID id) {
+
+    }
+
+    @Override
+    public List<T> findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean existsById(ID id) {
+        return false;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(T entity) {
+
+    }
+}
