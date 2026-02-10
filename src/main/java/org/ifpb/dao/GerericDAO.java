dac11bc676c1d63d900d00f7483879871cbe5aa7
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-10 17:50:33 -0300

    refactor: remove unused update method from GenericDAO

--- src/main/java/org/ifpb/dao/GerericDAO.java
+++ src/main/java/org/ifpb/dao/GerericDAO.java
@@ -4,7 +4,6 @@ public interface GerericDAO<T, ID>{
     T save(T entity);
     T findById(ID id);
     void deleteById(ID id);
-    T update(T entity);
     Iterable<T> findAll();
     boolean existsById(ID id);
     long count();
