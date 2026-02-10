1f453224219893be2ca16d53073601bf7b2ec6b7
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-10 18:15:43 -0300

    refactor: update ConsultaDAO to implement and extends GenericDAOImpl, IConsultaDAO and remove deprecated methods

--- src/main/java/org/ifpb/dao/ConsultaDAO.java
+++ src/main/java/org/ifpb/dao/ConsultaDAO.java
@@ -1,51 +1,12 @@
 package org.ifpb.dao;
 
-import jakarta.persistence.EntityManager;
-import org.ifpb.config.HibernateUtil;
+
 import org.ifpb.model.Consulta;
 
-public class ConsultaDAO implements GerericDAO<Consulta, Long>{
+public class ConsultaDAO extends GenericDAOImpl<Consulta, Long> implements IConsultaDAO {
 
-    private EntityManager getEntityManager() {
-        return HibernateUtil.getEntityManager();
+    public ConsultaDAO() {
+        super(Consulta.class);
     }
 
-    public Consulta save(Consulta entity) {
-        return null;
-    }
-
-
-    public Consulta findById(Long i) {
-        return null;
-    }
-
-
-    public void deleteById(Long i) {
-
-    }
-
-
-    public Consulta update(Consulta entity) {
-        return null;
-    }
-
-
-    public Iterable<Consulta> findAll() {
-        return null;
-    }
-
-
-    public boolean existsById(Long i) {
-        return false;
-    }
-
-
-    public long count() {
-        return 0;
-    }
-
-
-    public void delete(Consulta entity) {
-
-    }
 }
