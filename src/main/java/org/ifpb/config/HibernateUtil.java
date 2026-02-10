bd8c66a8be831556c74688b24f3506cbfc943d89
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-10 17:47:37 -0300

    feat: add methods to close the EntityManagerFactory

--- src/main/java/org/ifpb/config/HibernateUtil.java
+++ src/main/java/org/ifpb/config/HibernateUtil.java
@@ -7,11 +7,14 @@ import jakarta.persistence.Persistence;
 public class HibernateUtil {
     private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("sgcm-pu");
 
-    public static EntityManagerFactory getFactory() {
+    public static  EntityManagerFactory getFactory() {
         return factory;
     }
 
     public static EntityManager getEntityManager() {
         return factory.createEntityManager();
+    }
+    public static void closeFactory() {
+        factory.close();
     }
 }
