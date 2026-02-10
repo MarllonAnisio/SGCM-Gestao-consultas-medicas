f3e48b4009ae8159e024853a6bef06f0f6318d59
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-10 17:51:28 -0300

    feat: add MedicoDAO implementation and findByCrm method

---
+++ src/main/java/org/ifpb/dao/MedicoDAO.java
@@ -0,0 +1,16 @@
+package org.ifpb.dao;
+
+import org.ifpb.model.Medico;
+
+public class MedicoDAO extends GenericDAOImpl<Medico, Long> implements IMedicoDAO{
+
+    public MedicoDAO(Class<Medico> classe) {
+        super(classe);
+    }
+
+    @Override
+    public Medico findByCrm(String crm) {
+        return null;
+    }
+
+}
