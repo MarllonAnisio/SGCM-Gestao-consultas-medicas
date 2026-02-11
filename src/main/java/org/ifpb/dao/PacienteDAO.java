582d5f8589b3bc7f6fce8a21377d5eb3a917300b
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-11 14:30:48 -0300

    feat: add findByCpf method to PacienteDAO

---
+++ src/main/java/org/ifpb/dao/PacienteDAO.java
@@ -0,0 +1,23 @@
+package org.ifpb.dao;
+
+import org.ifpb.model.Paciente;
+
+public class PacienteDAO extends GenericDAOImpl<Paciente, Long> implements IPacienteDAO {
+
+
+    public PacienteDAO() {
+        super(Paciente.class);
+    }
+
+    public Paciente findByCpf(String cpf){
+        try{
+            String jpql = "SELECT p FROM tb_paciente p WHERE p.cpf = :cpf";
+            return getEntityManager()
+                    .createQuery(jpql, Paciente.class).
+                    setParameter("cpf", cpf).
+                    getSingleResult();
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
