81acd654ad13da95cf2c65851e5615d199c420ce
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-12 16:30:18 -0300

    feat: add toString, hashCode, and equals methods for Consulta

--- src/main/java/org/ifpb/model/Consulta.java
+++ src/main/java/org/ifpb/model/Consulta.java
@@ -4,9 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.ifpb.dto.consulta.ConsultaRequestDTO;
 import org.ifpb.model.enums.StatusConsulta;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -33,4 +36,28 @@ public class Consulta {
 
     @ManyToOne
     private Paciente paciente;
+
+    @Override
+    public String toString() {
+        return "Consulta{" +
+                "id=" + id +
+                ", data=" + data +
+                ", observacao='" + observacao + '\'' +
+                ", status=" + status +
+                ", medico=" + medico.getNome() +
+                ", paciente=" + paciente.getNome() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Consulta consulta = (Consulta) o;
+        return Objects.equals(id, consulta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
