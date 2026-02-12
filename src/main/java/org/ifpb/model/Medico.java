be81344f498f82b44fdf602d266fe0d8771a1cb4
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-12 15:26:34 -0300

    feat: add constructor for MedicoRequestDTO and update Medico class to use MedicoRequestDTO for creation

--- src/main/java/org/ifpb/model/Medico.java
+++ src/main/java/org/ifpb/model/Medico.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.ifpb.dto.medico.MedicoRequestDTO;
 import org.ifpb.model.enums.Especialidade;
 
 import java.util.List;
@@ -34,4 +35,22 @@ public class Medico {
 
     @OneToMany(mappedBy = "medico", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Consulta> consultas;
+
+    public Medico(){
+
+    }
+
+    public Medico(Long id, String CRM, String nome, Especialidade especialidade) {
+        this.id = id;
+        this.CRM = CRM;
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Medico(MedicoRequestDTO medicoRequestDTO) {
+        this.CRM = medicoRequestDTO.getCRM();
+        this.nome = medicoRequestDTO.getNome();
+        this.especialidade = medicoRequestDTO.getEspecialidade();
+    }
+
 }
