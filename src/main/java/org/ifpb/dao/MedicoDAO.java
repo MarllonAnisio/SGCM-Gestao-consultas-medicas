b3b6ebbe69d5d1e99cc7f7b6aa0f2c5043eec140
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-12 17:16:15 -0300

    feat: add findByCrm method to return Optional<Medico>

--- src/main/java/org/ifpb/dao/MedicoDAO.java
+++ src/main/java/org/ifpb/dao/MedicoDAO.java
@@ -1,7 +1,12 @@
 package org.ifpb.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import org.ifpb.dao.interfaces.IMedicoDAO;
 import org.ifpb.model.Medico;
+
+import java.util.List;
+import java.util.Optional;
 
 public class MedicoDAO extends GenericDAOImpl<Medico, Long> implements IMedicoDAO {
 
@@ -10,8 +15,18 @@ public class MedicoDAO extends GenericDAOImpl<Medico, Long> implements IMedicoDA
     }
 
     @Override
-    public Medico findByCrm(String crm) {
-        return null;
-    }
+    public Optional<Medico> findByCrm(String crm) {
 
+        try (EntityManager em = getEntityManager()) {
+
+            String jpql = "SELECT m FROM Medico m WHERE m.CRM = :crm";
+
+            List<Medico> resultado = em.createQuery(jpql, Medico.class)
+                    .setParameter("crm", crm)
+                    .setMaxResults(1)
+                    .getResultList();
+
+            return resultado.isEmpty() ? Optional.empty() : Optional.of(resultado.get(0));
+        }
+    }
 }
