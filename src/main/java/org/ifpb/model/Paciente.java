c5a0791defff9b525d87d299c97fd4e86f214e59
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-13 18:50:13 -0300

    refactor: update imports and add lombok annotations for Paciente entity

--- src/main/java/org/ifpb/model/Paciente.java
+++ src/main/java/org/ifpb/model/Paciente.java
@@ -1,11 +1,20 @@
 package org.ifpb.model;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import org.ifpb.dto.paciente.PacienteRequestDTO;
 
 import java.util.List;
 import java.util.Objects;
