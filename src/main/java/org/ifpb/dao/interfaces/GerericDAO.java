aecb376b6c392e07120106fbcf47128a9d7a7c12
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-13 00:48:50 -0300

    refactor: update method signatures and return types for consistency

--- src/main/java/org/ifpb/dao/interfaces/GerericDAO.java
+++ src/main/java/org/ifpb/dao/interfaces/GerericDAO.java
@@ -1,10 +1,14 @@
 package org.ifpb.dao.interfaces;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface GerericDAO<T, ID>{
-    T save(T entity);
-    T findById(ID id);
+    void save(T entity);
+    T update(T entity);
+    Optional<T> findById(ID id);
     void deleteById(ID id);
-    Iterable<T> findAll();
+    List<T> findAll();
     boolean existsById(ID id);
     long count();
     void delete(T entity);
