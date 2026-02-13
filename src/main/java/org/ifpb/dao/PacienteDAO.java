94243a95348c175e0ef70e27fc073b10ec24d382
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-13 01:05:29 -0300

    feat: refactor findByCpf method to use EntityManager injection and cleanup

--- src/main/java/org/ifpb/dao/PacienteDAO.java
+++ src/main/java/org/ifpb/dao/PacienteDAO.java
@@ -1,7 +1,11 @@
 package org.ifpb.dao;
 
+import jakarta.persistence.EntityManager;
 import org.ifpb.dao.interfaces.IPacienteDAO;
 import org.ifpb.model.Paciente;
+
+import java.util.List;
+import java.util.Optional;
 
 public class PacienteDAO extends GenericDAOImpl<Paciente, Long> implements IPacienteDAO {
 
@@ -10,15 +14,17 @@ public class PacienteDAO extends GenericDAOImpl<Paciente, Long> implements IPaci
         super(Paciente.class);
     }
 
-    public Paciente findByCpf(String cpf){
-        try{
+    public Optional<Paciente> findByCpf(String cpf){
+        try(EntityManager em = getEntityManager()){
             String jpql = "SELECT p FROM Paciente p WHERE p.cpf = :cpf";
-            return getEntityManager()
-                    .createQuery(jpql, Paciente.class).
-                    setParameter("cpf", cpf).
-                    getSingleResult();
-        }catch (Exception e){
-            return null;
+
+            List<Paciente> resultado = em.createQuery(jpql, Paciente.class)
+                    .setParameter("cpf", cpf)
+                    .setMaxResults(1)
+                    .getResultList();
+
+            return resultado.isEmpty() ? Optional.empty() : Optional.of(resultado.get(0));
+
         }
     }
 }
