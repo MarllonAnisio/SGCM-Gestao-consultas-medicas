b32a82003d85c9541aea6be15526f21a6f9c8c65
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-13 15:01:22 -0300

    feat: reorganizing imports

--- src/main/java/org/ifpb/model/Consulta.java
+++ src/main/java/org/ifpb/model/Consulta.java
@@ -1,6 +1,17 @@
 package org.ifpb.model;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
