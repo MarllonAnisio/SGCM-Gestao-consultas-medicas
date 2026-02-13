d78f715e1e70d55bad44c1938faa0cc52414662b
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-13 20:30:04 -0300

    feat: add methods to filter and retrieve medico entities based on status

--- src/main/java/org/ifpb/dao/interfaces/IMedicoDAO.java
+++ src/main/java/org/ifpb/dao/interfaces/IMedicoDAO.java
@@ -2,8 +2,13 @@ package org.ifpb.dao.interfaces;
 
 import org.ifpb.model.Medico;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IMedicoDAO extends GerericDAO<Medico, Long> {
     Optional<Medico> findByCrm(String crm);
+    List<Medico> findAllAtivos();
+    List<Medico> findAllInativos();
+    Optional<Medico> findByIdInclusiveInativos(Long id);
+    Optional<Medico> findByIdAtivo(Long id);
 }
