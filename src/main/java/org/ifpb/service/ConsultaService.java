ddd6915a20506078894e1438259a421e617a0b7d
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-13 15:35:31 -0300

    feat: add agendar method to ConsultaService and update imports

--- src/main/java/org/ifpb/service/ConsultaService.java
+++ src/main/java/org/ifpb/service/ConsultaService.java
@@ -3,6 +3,12 @@ package org.ifpb.service;
 import org.ifpb.dao.ConsultaDAO;
 import org.ifpb.dao.MedicoDAO;
 import org.ifpb.dao.PacienteDAO;
+import org.ifpb.dto.consulta.ConsultaRequestDTO;
+import org.ifpb.model.Consulta;
+import org.ifpb.model.Medico;
+import org.ifpb.model.Paciente;
+import org.ifpb.service.service_exeptions.MedicoNaoEncontradoExeption;
+import org.ifpb.service.service_exeptions.PacienteNaoEncontradoExeption;
 
 
 public class ConsultaService {
@@ -16,7 +22,26 @@ public class ConsultaService {
         medicoDAO = new MedicoDAO();
         pacienteDAO = new PacienteDAO();
     }
+    public void agendar(ConsultaRequestDTO consultaDTO) throws MedicoNaoEncontradoExeption, PacienteNaoEncontradoExeption {
 
+        Medico medico = medicoDAO.findById(consultaDTO.getIdMedico())
+                .orElseThrow(() -> new MedicoNaoEncontradoExeption("Medico não encontrado"));
+
+        Paciente paciente = pacienteDAO.findById(consultaDTO.getIdPaciente())
+                .orElseThrow(() -> new PacienteNaoEncontradoExeption("Paciente não encontrado"));
+
+        Consulta consulta = Consulta.builder()
+                .data(consultaDTO.getData())
+                .observacao(consultaDTO.getObservacao())
+                .medico(medico).
+                paciente(paciente).
+                build();
+
+        consulta.setMedico(medico);
+        consulta.setPaciente(paciente);
+
+        consultaDAO.save(consulta);
+    }
 
 }
 
