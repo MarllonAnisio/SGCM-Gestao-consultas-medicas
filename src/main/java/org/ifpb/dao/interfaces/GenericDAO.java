57a57e9a342b4c01f0672b03e76de227bb76eeaa
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-15 02:14:45 -0300

    refactor: rename GenericDAO interface to align with naming conventions

--- src/main/java/org/ifpb/dao/interfaces/GenericDAO.java
+++ src/main/java/org/ifpb/dao/interfaces/GenericDAO.java
@@ -3,7 +3,7 @@ package org.ifpb.dao.interfaces;
 import java.util.List;
 import java.util.Optional;
 
-public interface GerericDAO<T, ID>{
+public interface GenericDAO<T, ID>{
     void save(T entity);
     T update(T entity);
     Optional<T> findById(ID id);
