48e47d84b89fbb540ec1d1ab6aa7f3e4b4da8e36
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-17 22:29:06 -0300

    feat: add validation annotations to Paciente entity fields for improved data integrity

--- src/main/java/org/ifpb/model/Paciente.java
+++ src/main/java/org/ifpb/model/Paciente.java
@@ -10,12 +10,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.validator.constraints.br.CPF;
 import org.ifpb.model.model_interfaces.IExclusaoLogica;
 
 import java.time.LocalDate;
@@ -23,8 +30,8 @@ import java.util.List;
 import java.util.Objects;
 
 @NoArgsConstructor
-@Getter @Setter
 @AllArgsConstructor
+@Getter @Setter
 @Builder
 @Entity
 @Table(name = "tb_pacientes")
@@ -35,23 +42,38 @@ public class Paciente implements IExclusaoLogica {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Nome é Obrigatório")
+    @Size(message = "Nome deve ter entre  e 100 caracteres", min = 3, max = 100)
+    @Column(nullable = false, length = 100)
     private String nome;
 
+    @CPF
+    @NotBlank(message = "CPF é Obrigatório")
     @Column(length=11, nullable=false, unique = true)
     private String cpf;
 
+    @Pattern(
+            regexp = "^\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}$",
+            message = "Telefone inválido. Formato: (11) 91234-5678"
+    )
     private String telefone;
 
     @Builder.Default
     @Column(nullable = false)
     private boolean ativo = true;
 
+    @Email(message = "Email inválido")
+    @NotBlank(message = "O Paciente deve ter email")
     @Column(nullable = false, unique = true)
     private String email;
-    @Column(nullable = false)
-    private LocalDate datanascimento;
 
-    @OneToMany(mappedBy = "paciente", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @NotNull(message = "Data de Nascimento é Obrigatória")
+    @Past(message = "Data de Nascimento deve ser anterior a data atual")
+    @Column(nullable = false, name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "paciente", fetch = FetchType.LAZY)
     private List<Consulta> consultas;
 
 
@@ -76,7 +98,7 @@ public class Paciente implements IExclusaoLogica {
                 ", cpf='" + cpf + '\'' +
                 ", telefone='" + telefone + '\'' +
                 ", email='" + email + '\'' +
-                ", datanascimento=" + datanascimento +
+                ", datanascimento=" + dataNascimento +
                 '}';
     }
 
