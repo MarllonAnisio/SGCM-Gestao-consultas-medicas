940222b618595ca2cc552fee0dd39f484ae81477
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-20 19:05:19 -0300

    feat: add method findAllInativos and refactor findByCpf

--- src/main/java/org/ifpb/dao/PacienteDAO.java
+++ src/main/java/org/ifpb/dao/PacienteDAO.java
@@ -1,6 +1,7 @@
 package org.ifpb.dao;
 
 import jakarta.persistence.EntityManager;
+import org.ifpb.dao.dao_exceptions.DatabaseException;
 import org.ifpb.dao.interfaces.IPacienteDAO;
 import org.ifpb.model.Paciente;
 
@@ -14,17 +15,58 @@ public class PacienteDAO extends GenericDAOImpl<Paciente, Long> implements IPaci
         super(Paciente.class);
     }
 
-    public Optional<Paciente> findByCpf(String cpf){
-        try(EntityManager em = getEntityManager()){
+    public Optional<Paciente> findByCpf(String cpf) {
+        try (EntityManager em = getEntityManager()) {
             String jpql = "SELECT p FROM Paciente p WHERE p.cpf = :cpf";
-
-            List<Paciente> resultado = em.createQuery(jpql, Paciente.class)
+            return em.createQuery(jpql, Paciente.class)
                     .setParameter("cpf", cpf)
                     .setMaxResults(1)
-                    .getResultList();
+                    .getResultStream()
+                    .findFirst();
+        } catch (Exception e) {
+            throw new DatabaseException("Erro ao buscar paciente pelo CPF: " + cpf, e);
+        }
+    }
 
-            return resultado.isEmpty() ? Optional.empty() : Optional.of(resultado.get(0));
+    public List<Paciente> findAllAtivos() {
+        try (EntityManager em = getEntityManager()) {
+            String jpql = "SELECT p FROM Paciente p WHERE p.ativo = true";
+            return em.createQuery(jpql, Paciente.class).getResultList();
 
+        } catch (Exception e) {
+            throw new DatabaseException("Erro ao listar médicos ativos.", e);
+        }
+    }
+
+
+    public List<Paciente> findAllInativos() {
+        try (EntityManager em = getEntityManager()) {
+            String jpql = "SELECT p FROM Paciente p WHERE p.ativo = false";
+            return em.createQuery(jpql, Paciente.class).getResultList();
+
+        } catch (Exception e) {
+            throw new DatabaseException("Erro ao listar Pacientes inativos.", e);
+        }
+    }
+
+
+    public Optional<Paciente> findByIdInclusiveInativos(Long id) {
+        return super.findById(id);
+    }
+
+
+    public Optional<Paciente> findByIdAtivo(Long id) {
+        try (EntityManager em = getEntityManager()) {
+            String jpql = "SELECT p FROM Paciente p WHERE p.ativo = true AND p.id = :id";
+
+            return em.createQuery(jpql, Paciente.class)
+                    .setParameter("id", id)
+                    .setMaxResults(1)
+                    .getResultStream()
+                    .findFirst();
+
+        } catch (Exception e) {
+            throw new DatabaseException("Erro ao buscar Paciente ativo por ID.", e);
         }
     }
 }
